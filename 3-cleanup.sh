5e63abf18c02c4a8af0c75afe969e87307060336
Tan Chang Kiat <tanchangkiat@gmail.com>
2023-02-21 17:42:43 +0800

    Added code to query Kubernetes API

--- 3-cleanup.sh
+++ 3-cleanup.sh
@@ -8,12 +8,7 @@ STACK=amazon-eks-k8s-api
 BUCKET_NAME=check-eks-cluster-assets
 FUNCTION=check-eks-cluster
 
-echo "--- Deleting stack $STACK ---"
 aws cloudformation delete-stack --stack-name $STACK
 rm out.yml
-echo "--- Deleteing bucket $BUCKET_NAME ---"
 aws s3 rb --force s3://$BUCKET_NAME
-rm bucket-name.txt
-
-echo "--- Deleting log group '/aws/lambda/$FUNCTION' ---"
 aws logs delete-log-group --log-group-name /aws/lambda/$FUNCTION
--- functions/check-eks-cluster.py
+++ functions/check-eks-cluster.py
@@ -1,5 +1,20 @@
+"Get information from EKS clusters"
+import base64
 import boto3
 import json
+import logging
+import re
+
+from botocore.signers import RequestSigner
+from kubernetes import client, config
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+STS_TOKEN_EXPIRES_IN = 60
+session = boto3.session.Session()
+sts = session.client('sts')
+service_id = sts.meta.service_model.service_id
 
 def lambda_handler(event, context):
     eks = boto3.client('eks')
@@ -9,8 +24,8 @@ def lambda_handler(event, context):
     for clusterName in eks.list_clusters()['clusters']:
         cluster = eks.describe_cluster(
             name=clusterName
-        )
-        cluster_response = { 'name': cluster['cluster']['name'], 'status': cluster['cluster']['status'], 'version': cluster['cluster']['version'] }
+        )['cluster']
+        cluster_response = { 'name': cluster['name'], 'status': cluster['status'], 'version': cluster['version'] }
         
         nodegroup_response = []
         for nodegroupName in eks.list_nodegroups(clusterName=clusterName)['nodegroups']:
@@ -19,10 +34,64 @@ def lambda_handler(event, context):
                 nodegroupName=nodegroupName
             )
             nodegroup_response.append({ 'name': ng['nodegroup']['nodegroupName'], 'status': ng['nodegroup']['status'], 'version': ng['nodegroup']['version'] })
-            
-        response.append({ 'cluster': cluster_response, 'nodegroups': nodegroup_response, })
+        
+        kubeconfig = {
+            'apiVersion': 'v1',
+            'clusters': [{
+            'name': 'cluster1',
+            'cluster': {
+                'certificate-authority-data': cluster["certificateAuthority"]["data"],
+                'server': cluster["endpoint"]}
+            }],
+            'contexts': [{'name': 'context1', 'context': {'cluster': 'cluster1', "user": "user1"}}],
+            'current-context': 'context1',
+            'kind': 'Config',
+            'preferences': {},
+            'users': [{'name': 'user1', "user" : {'token': get_bearer_token(clusterName)}}]
+        }
+
+        # config.load_kube_config_from_dict(config_dict=kubeconfig)
+        # print(kubeconfig)
+        # v1 = client.CoreV1Api()
+        # ret = v1.list_pod_for_all_namespaces(watch=False)
+        # print(len(ret.items))
+
+        response.append({ 'cluster': cluster_response, 'nodegroups': nodegroup_response })
     
     return {
         'statusCode': 200,
         'body': json.dumps(response)
     }
+
+def get_bearer_token(clusterName):
+    "Create authentication token"
+    signer = RequestSigner(
+        service_id,
+        session.region_name,
+        'sts',
+        'v4',
+        session.get_credentials(),
+        session.events
+    )
+
+    params = {
+        'method': 'GET',
+        'url': 'https://sts.{}.amazonaws.com/'
+               '?Action=GetCallerIdentity&Version=2011-06-15'.format(session.region_name),
+        'body': {},
+        'headers': {
+            'x-k8s-aws-id': clusterName
+        },
+        'context': {}
+    }
+
+    signed_url = signer.generate_presigned_url(
+        params,
+        region_name=session.region_name,
+        expires_in=STS_TOKEN_EXPIRES_IN,
+        operation_name=''
+    )
+    base64_url = base64.urlsafe_b64encode(signed_url.encode('utf-8')).decode('utf-8')
+
+    # remove any base64 encoding padding:
+    return 'k8s-aws-v1.' + re.sub(r'=*', '', base64_url)
