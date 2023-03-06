c5bccedef5f71a5a88094632aa30919baadd5562
Tan Chang Kiat <tanchangkiat@gmail.com>
2023-03-06 15:12:34 +0800

    Fix unauthorized error when using Kubernetes client

--- functions/check-eks-cluster.py
+++ functions/check-eks-cluster.py
@@ -11,36 +11,42 @@ from kubernetes import client, config
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+REGION = "ap-southeast-1"
 STS_TOKEN_EXPIRES_IN = 60
 TEAM_MEMBER_ACCOUNTS = ["409989946510"]
 TEAM_ROLE = ["arn:aws:iam::409989946510:role/gameday-quest-TeamRole"]
 
-session = boto3.session.Session()
-sts = session.client('sts')
-service_id = sts.meta.service_model.service_id
+# session = boto3.session.Session()
+# sts = session.client('sts')
 
 def lambda_handler(event, context):
+    sts = boto3.client('sts')
+
     # Assume role in Team account
-    sts_connection = boto3.client('sts')
-    team_acct = sts_connection.assume_role(
+    team_account = sts.assume_role(
         RoleArn=TEAM_ROLE[0],
         RoleSessionName="assume_team_role"
     )
 
+    # Initiate a boto3 client for EKS with the credentials of the team account
     eks = boto3.client('eks',
-        aws_access_key_id = team_acct['Credentials']['AccessKeyId'],
-        aws_secret_access_key = team_acct['Credentials']['SecretAccessKey'],
-        aws_session_token = team_acct['Credentials']['SessionToken']
+        aws_access_key_id = team_account['Credentials']['AccessKeyId'],
+        aws_secret_access_key = team_account['Credentials']['SecretAccessKey'],
+        aws_session_token = team_account['Credentials']['SessionToken'],
+        region_name = REGION
     )
     
     response = []
     
+    # Traverse through the EKS clusters in a team account to get the list of Nodegroups and Pods
     for clusterName in eks.list_clusters()['clusters']:
+        # Get EKS cluster data
         cluster = eks.describe_cluster(
             name=clusterName
         )['cluster']
         cluster_response = { 'name': cluster['name'], 'status': cluster['status'], 'version': cluster['version'] }
 
+        # Get nodegroups data
         nodegroup_response = []
         nodegroups = eks.list_nodegroups(clusterName=clusterName)['nodegroups']
         for nodegroupName in nodegroups:
@@ -50,6 +56,7 @@ def lambda_handler(event, context):
             )
             nodegroup_response.append({ 'name': ng['nodegroup']['nodegroupName'], 'status': ng['nodegroup']['status'], 'version': ng['nodegroup']['version'] })
         
+        # Set up Kubernetes client
         kubeconfig = {
             'apiVersion': 'v1',
             'clusters': [{
@@ -62,13 +69,22 @@ def lambda_handler(event, context):
             'current-context': 'context1',
             'kind': 'Config',
             'preferences': {},
-            'users': [{'name': 'user1', "user" : {'token': get_bearer_token(clusterName)}}]
+            'users': [{'name': 'user1', "user" : {'token': 
+                get_bearer_token(clusterName, sts, boto3.session.Session(
+                    aws_access_key_id = team_account['Credentials']['AccessKeyId'],
+                    aws_secret_access_key = team_account['Credentials']['SecretAccessKey'],
+                    aws_session_token = team_account['Credentials']['SessionToken'],
+                    region_name = REGION
+                )
+            )}}]
         }
-
         config.load_kube_config_from_dict(config_dict=kubeconfig)
         v1 = client.CoreV1Api()
+
+        # Get Pods from all namespaces
         system_pods = []
-        for p in v1.list_pod_for_all_namespaces(watch=False).items:
+        pod_list = v1.list_pod_for_all_namespaces(watch=False).items
+        for p in pod_list:
             system_pods.append({ 'name': p.metadata.name, 'image': p.spec.containers[0].image })
 
         response.append({ 'cluster': cluster_response, 'nodegroups': nodegroup_response, 'systemPods': system_pods })
@@ -78,8 +94,10 @@ def lambda_handler(event, context):
         'body': json.dumps(response)
     }
 
-def get_bearer_token(clusterName):
+def get_bearer_token(clusterName, sts, session):
     "Create authentication token"
+    service_id = sts.meta.service_model.service_id
+
     signer = RequestSigner(
         service_id,
         session.region_name,
@@ -88,6 +106,7 @@ def get_bearer_token(clusterName):
         session.get_credentials(),
         session.events
     )
+    print(signer)
 
     params = {
         'method': 'GET',
